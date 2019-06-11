62c16d329bfac29db79de6b86dab505fb795d27a
rms <rmnsfx@yandex.ru>
2019-06-11 15:22:17 +0500

    edit play

--- play.py
+++ play.py
@@ -8,85 +8,119 @@ import datetime
 from threading import Thread
 from bs4 import BeautifulSoup
 import urllib.request 
+import random
                         
 
 app = Flask(__name__)
 app.config['TESTING'] = False
 
-url = [
-
-"https://www.youtube.com/watch?v=LUOIvT9hzD8&list=RDLUOIvT9hzD8&index=1",
-"https://www.youtube.com/watch?v=iQCNvkEjX9c&list=RDLUOIvT9hzD8&index=5", 
-"https://www.youtube.com/watch?v=w4LkSRXrK34&list=RDLUOIvT9hzD8&index=14", 
-
+name_list = [
+"Flume+ezra",
+"Rawls+Errday+99",
+"Hollow+Clouds+Lopez+Theme",
+"Devotion+Uppermost",
+"Daft+Punk+Revolution+909",
+"Miike+Snow+Genghis+Khan",
+"Air+Playground+love",
+"Gorillaz+Empire+Ants",
+"Tame+Impala+The+less+I+know+the+better",
+"Tame+Impala+Let+it+happen",
+"Washed+out+Feel+it+all+around",
+"Foster+the+people+Dont+stop-remix",
+"Michael+Gray+Thee+Weekend",
+"David+Morales+Needin+U",
+"Air+La+femme+argent",
+"Tim+Deluxe+It+just+wont+do",
+"Junior+Jack+E+samba",
+"Michael+Gray+Borderline",
+"Axwell+Watch+the+sunrise",
+"C+Mos+2+Million+ways+axwell+remix",
+"Laurent+Wolf+Calinda",
+"Rui+Da+Silva+Touch+me",
+"Salome+de+bahia+Outro+lugar",
+"Basement+Jaxx+Bingo+Bango",
 ]
-
-videolist=[]
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     
-    Instance = None   
+    if request.method == 'GET':
+        return render_template('main_play.html') 
     
     # title = None
     # duration = None
-    yt_crawler()
     
-    if request.method == 'POST':
-        if request.form.get('Encrypt') == 'Play':            
-            if Instance == None:
-                Instance = vlc.Instance('--no-video')
+    while True:              
+                
+        Instance = None   
+        
+        videolist = yt_crawler(random.choice(name_list))        
+        
+        
+        if request.method == 'POST':                   
             
-                for item in videolist:
-                    
-                    try:
-                        video = pafy.new(item)
-                        best = video.getbest()
-                        playurl = best.url                    
+            if request.form.get('Encrypt') == 'Play':                            
+                
+                if Instance == None:             
+                                    
+                    Instance = vlc.Instance('--no-video')                   
+                                    
+                    for item in videolist[0:1]:
+                        
+                        try:
+                            video = pafy.new(item)
+                            best = video.getbest()
+                            playurl = best.url                    
 
-                    except:                                
-                        print('Error link')
-                    
-                    else:
-                        print(video.title, video.duration)
+                        except:                                
+                            print('Error link')
                         
-                        player = Instance.media_player_new()
-                        Media = Instance.media_new(best.url)
-                        Media.get_mrl()
-                        player.set_media(Media)                
-                        player.audio_set_volume(100)
-                        player.play()                     
+                        else:
+                            print(video.title, video.duration)
+                            
+                            player = Instance.media_player_new()
+                            Media = Instance.media_new(best.url)
+                            Media.get_mrl()
+                            player.set_media(Media)                
+                            player.audio_set_volume(100)
+                            player.play()                     
+                            
+                            # thread = Thread(target = render_title, args = (video.title, video.duration, ))
+                            # thread.daemon = True
+                            # thread.start()
+                            # thread.join()
+                            
+                            pt = datetime.datetime.strptime(video.duration,'%H:%M:%S')
+                            total_seconds = pt.second+pt.minute*60+pt.hour*3600
+                            
+                            if total_seconds < 300:
+                               time.sleep(total_seconds-5)                             
+                            else:
+                               time.sleep(300)                             
+                            
+                            
+                            player.stop()               
                         
-                        # thread = Thread(target = render_title, args = (video.title, video.duration, ))
-                        # thread.daemon = True
-                        # thread.start()
-                        # thread.join()
-                        
-                        pt = datetime.datetime.strptime(video.duration,'%H:%M:%S')
-                        total_seconds = pt.second+pt.minute*60+pt.hour*3600
-                        
-                        time.sleep(60)                             
-                        # time.sleep(total_seconds - 3)                             
-                        # player.audio_set_volume(50)                        
-                        # time.sleep(2)                
-                        player.stop()               
-                    
-                        #render_template('main_play.html', title = video.title, duration = video.duration)
+                            # render_template('main_play.html', title = video.title, duration = video.duration)
+                            
                     
     return render_template('main_play.html') 
 
-def yt_crawler():
+def yt_crawler(name):
 
-    base = "https://www.youtube.com/results?search_query="
-    qstring = "space+disco"
+    videolist = []
     
-    data = urllib.request.urlopen(base+qstring).read()
-    soup = BeautifulSoup(data)
+    base = "https://www.youtube.com/results?search_query="       
+    qstring = name
+    
+    req = urllib.request.Request(base+qstring)
+    req.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)')    
+    data = urllib.request.urlopen(req).read()
+    soup = BeautifulSoup(data)  
     
     
-    
-    for link in soup.findAll('a', attrs={'class':'yt-uix-tile-link'}):
-        #print(link['href'])
+    for link in soup.findAll('a', attrs={'class':'yt-uix-tile-link'})[0:1]:
+        # print(link['href'])
         tmp = 'https://www.youtube.com' + link['href']
         videolist.append(tmp)
         
@@ -96,19 +130,7 @@ def yt_crawler():
     return videolist        
         
             
-    
-    
 
-    # soup = BeautifulSoup(plain_text, 'html.parser')
-    
-    # for link in soup.findAll('a',{'class':'yt-uix-tile-link'}):
-        # lst.append('https://www.youtube.com' + link.get('href'))
-        # print(lst)
-    
-
-# def render_title(title, duration):
-    # return render_template('main_play.html', title = title, duration = duration)
-    # print(1)
 
     
 if __name__ == "__main__":
